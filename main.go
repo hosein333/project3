0b2a65b6fd5f2cd8bf6f1e4974225957a25300f0
mohammadjavad <hossinbabayy1382@gmail.com>
2023-12-14 18:32:03 +0330

    if len(nID) == 10

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import "fmt"
 
 type NationalID string
 
+func IsNationalIDlid(nID NationalID) bool {
+	if len(nID) == 10 {
+		return true
+	}
+		return false
+}
+
+
 func main() {
 	var nID NationalID
 	nID = "1273922184"
 
-	fmt.Println("My National ID Number :", nID)
+	fmt.Println("My National ID Number %s is Vlid :%v", nID, IsNationalIDlid(nID))
 }
