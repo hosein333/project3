521195315a16d2667d94841dc5e506e737120448
mohammadjavad <hossinbabayy1382@gmail.com>
2023-12-15 20:12:47 +0330

    ncode 1

--- main.go
+++ main.go
@@ -2,29 +2,12 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"/home/mohammadjavad/ncode"
 )
 
-type NationalID string
-
-func (id NationalID) IsNationalIDlid(nID NationalID) bool {
-	if len(nID) == 10 {
-		for _, val := range nID {
-		    _, err := strconv.Atoi(string(val))
-		    if err != nil {
-			return false
-	}
-}
-
-		return true
-	}
-		return false
-}
-
-
 func main() {
-	var nID NationalID
+	var nID ncode.NationalID
 	nID = "127-221840"
 
-	fmt.Println("My National ID Number %s is Vlid :%v", nID, nID.IsNationalIDlid(nID))
+	fmt.Printf("My National ID Number %s is Vlid :%v", nID, nID.IsVlid(nID))
 }
