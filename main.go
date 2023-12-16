38a1e657bb649f0175409e48c19e961f58decbdd
mohammadjavad <hossinbabayy1382@gmail.com>
2023-12-16 16:45:42 +0330

    tashkhise sahih bodane code meli

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Printf("My National ID Number %s is Vlid :%v\n", nID1, nID1.IsVlid())
 
         var nID2 ncode.NationalID
-        nID2 = "1270221840"
+        nID2 = "1274022185"
 
         fmt.Printf("My National ID Number %s is Vlid :%v\n", nID2, nID2.IsVlid())
 
