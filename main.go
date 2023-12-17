addbec744175793e27d252475742fadfabfe3b9b
mohammadjavad <hossinbabayy1382@gmail.com>
2023-12-17 15:05:41 +0330

    kholase tar

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 
 func main() {
 	var nID1 ncode.NationalID
-	nID1.StringFormat = "127-221840"
+	nID1.StringFormat = "1273588291"
 
 	fmt.Printf("My National ID Number %s is Vlid :%v\n", nID1.StringFormat, nID1.IsVlid())
 
         var nID2 ncode.NationalID
-        nID2.StringFormat = "1274022185"
+        nID2.StringFormat = "1274022184"
 
         fmt.Printf("My National ID Number %s is Vlid :%v\n", nID2.StringFormat, nID2.IsVlid())
 
