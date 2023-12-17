e28f9e2b2864235e9e729eac2330d0a3a595c9a0
mohammadjavad <hossinbabayy1382@gmail.com>
2023-12-17 15:54:22 +0330

    undefined: fmt

--- main.go
+++ main.go
@@ -6,14 +6,15 @@ import (
 )
 
 func main() {
-	var nID1 ncode.NationalID
-	nID1.StringFormat = "1273588291"
+	var nID1 lib.NationalID
+	nID1.StringFormat = "1274022184"
 
 	fmt.Printf("My National ID Number %s is Vlid :%v\n", nID1.StringFormat, nID1.IsVlid())
 
-        var nID2 ncode.NationalID
-        nID2.StringFormat = "1274022184"
+	nID2 := &lib.NationalID{StringFormat : "1274022185"}
 
         fmt.Printf("My National ID Number %s is Vlid :%v\n", nID2.StringFormat, nID2.IsVlid())
+
+	fmt.Println("OutClass, IsValid: ", nID2.IsValid)
 
 }
