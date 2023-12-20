567c753eeace9147cafd22d8433f508bfca039a5
mohammadjavad <hossinbabayy1382@gmail.com>
2023-12-20 18:52:32 +0330

    hide stringFormat and digitFormat

--- main.go
+++ main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"fmt"
-	"project3/lib.go"
+	"project3/lib"
 )
 
 func main() {
-	var nID1 lib.NationalID
-	nID1.StringFormat = "1274022184"
 
-	fmt.Printf("My National ID Number %s is Vlid :%v\n", nID1.StringFormat, nID1.IsVlid())
+	nID1 := lib.NewNationalID("1274022185")
 
-	nID2 := &lib.NationalID{StringFormat : "1274022185"}
-
-        fmt.Printf("My National ID Number %s is Vlid :%v\n", nID2.StringFormat, nID2.IsVlid())
-
-	fmt.Println("OutClass, IsValid: ", nID2.IsValid)
+	fmt.Println("OutClass, IsValid: ", nID1.IsValid)
 
 }
